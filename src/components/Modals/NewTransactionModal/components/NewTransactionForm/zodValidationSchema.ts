3937f7250b02a24899fd11ed219f9b47a6d0782a
Lucas Felix <lucariozin.net@gmail.com>
2023-01-12 11:57:09 -0300

    separating zod error messages in another object.

--- src/components/Modals/NewTransactionModal/components/NewTransactionForm/zodValidationSchema.ts
+++ src/components/Modals/NewTransactionModal/components/NewTransactionForm/zodValidationSchema.ts
@@ -1,5 +1,13 @@
 import * as zod from 'zod'
 
+const errorMessages = {
+  transactionTypeError: 'O tipo da transação precisa ser uma "Entrada" ou uma "Saída"',
+  addDescriptionError: 'Adicione uma descrição',
+  addCategoryError: 'Adicione uma categoria',
+  addPriceError: 'Adicione um preço maior que 0',
+  maxTextLengthError: 'Ops, seu texto ultrapassou o limite permitido',
+}
+
 export const zodValidationSchema = zod.object({
   type: zod.string().refine((value) => {
     const possibilities = ['income', 'outcome']
@@ -7,8 +15,8 @@ export const zodValidationSchema = zod.object({
     if (!possibilities.includes(value)) return false
 
     return true
-  }, 'O tipo da transação precisa ser uma "Entrada" ou uma "Saída"'),
-  description: zod.string().min(1, 'Adicione uma descrição').max(100, 'Ops, seu texto ultrapassou o limite permitido'),
+  }, errorMessages.transactionTypeError),
+  description: zod.string().min(1, errorMessages.addDescriptionError).max(100, errorMessages.maxTextLengthError),
   price: zod
     .string()
     .transform((value) => Number(value))
@@ -18,6 +26,6 @@ export const zodValidationSchema = zod.object({
       if (valueIsNaN || value <= 0) return false
 
       return true
-    }, 'Adicione um preço maior que 0'),
-  category: zod.string().min(1, 'Adicione uma categoria').max(100, 'Ops, seu texto ultrapassou o limite permitido'),
+    }, errorMessages.addPriceError),
+  category: zod.string().min(1, errorMessages.addCategoryError).max(100, errorMessages.maxTextLengthError),
 })
